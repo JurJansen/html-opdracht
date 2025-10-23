e1360fd185d6bf0f6fd0043b357bdb5b8cb17692
jjansen123 <jurretje.jansen@gmail.com>
2025-10-23 13:27:03 +0200

    precies

---
+++ fullScreen.js
@@ -0,0 +1,27 @@
+const pres = document.querySelectorAll('pre');
+
+pres.forEach(pre => {
+  let mouseDownPos = null;
+
+  pre.addEventListener('mousedown', e => {
+    mouseDownPos = { x: e.clientX, y: e.clientY };
+  });
+
+  pre.addEventListener('mouseup', e => {
+    const dx = e.clientX - mouseDownPos.x;
+    const dy = e.clientY - mouseDownPos.y;
+    if (Math.sqrt(dx*dx + dy*dy) < 5) {
+      pres.forEach(p => {
+        if (p !== pre) p.classList.remove('fullscreen');
+      });
+
+      pre.classList.toggle('fullscreen');
+    }
+  });
+});
+
+document.addEventListener('keydown', (e) => {
+  if (e.key === "Escape") {
+    pres.forEach(pre => pre.classList.remove('fullscreen'));
+  }
+});
